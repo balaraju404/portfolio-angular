84022bd16ef90d11328bbe02189778ae0e2d7a1f
balaraju <balaraju@bijib.in>
2026-06-12 14:17:18 +0530

    feat: enhance about, contact, and home pages with improved layout and content; update CSS for better responsiveness

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { APP_ROUTES, LAYOUT_ROUTES } from '@constants/route.constants';
 
 @Component({
  selector: 'app-home',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
  templateUrl: './home.html',
 })
 export class Home {
+ private readonly router = inject(Router)
 
+ gotoPortfolio(): void {
+  this.router.navigate([APP_ROUTES.layout, LAYOUT_ROUTES.portfolio])
+ }
+ gotoLogin(): void {
+  this.router.navigate([APP_ROUTES.layout, LAYOUT_ROUTES.login])
+ }
 }
