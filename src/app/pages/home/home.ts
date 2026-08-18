12524b2dfefbd8cf5d352c89e1b967d0e3424971
balaraju <balaraju@bijib.in>
2026-08-18 15:07:48 +0530

    changes

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { APP_ROUTES, LAYOUT_ROUTES } from '@constants/route.constants';
+import { STORAGE_CONSTANTS } from '@constants/storage.constants';
+import { UserData } from '@core/api/login/login-api.interface';
 import { DashboardData } from '@core/api/public/public-api.interface';
 import { PublicAPI } from '@core/api/public/public-api.service';
+import { StorageService } from '@shared/services/storage.service';
 
 @Component({
  selector: 'app-home',
@@ -12,6 +15,7 @@ import { PublicAPI } from '@core/api/public/public-api.service';
 export class Home implements OnInit {
  private readonly router = inject(Router)
  private readonly publicAPI = inject(PublicAPI)
+ private readonly storageService = inject(StorageService)
 
  dashboardData: DashboardData = {
   portfolios_created: 0,
@@ -26,8 +30,14 @@ export class Home implements OnInit {
  gotoPortfolio(): void {
   this.router.navigate([APP_ROUTES.layout, LAYOUT_ROUTES.portfolio])
  }
- gotoLogin(): void {
-  this.router.navigate([APP_ROUTES.layout, LAYOUT_ROUTES.login])
+
+ getStartedEvent(): void {
+  const token = this.storageService.getItem<UserData>(STORAGE_CONSTANTS.token)
+  let path = LAYOUT_ROUTES.login
+  if (token) {
+   path = LAYOUT_ROUTES.portfolio_create
+  }
+  this.router.navigate([APP_ROUTES.layout, path])
  }
 
  private fetchDashboardData(): void {
